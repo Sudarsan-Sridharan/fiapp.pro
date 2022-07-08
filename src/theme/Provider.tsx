135032e6dcf5e4392833ed145f50ac7df847e22b
agdholo <agdholo@gmail.com>
2022-07-08 23:51:18 +0800

    fix(app): theme provider

--- src/theme/Provider.tsx
+++ src/theme/Provider.tsx
@@ -7,7 +7,7 @@ import { labTheme } from '@/theme/labTheme';
 import type { CustomThemeProviderProps } from './types';
 
 function CustomThemeProvider({ children }: CustomThemeProviderProps) {
-  return <ThemeProvider theme={labTheme('light')}>{children}</ThemeProvider>;
+  return <ThemeProvider theme={labTheme}>{children}</ThemeProvider>;
 }
 
 export default CustomThemeProvider;

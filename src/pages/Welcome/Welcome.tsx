99c23b05c465841bd7fe23158fbb7cad591d0df3
agdholo <agdholo@gmail.com>
2022-07-11 00:01:07 +0800

    Revert "chore(app): add risk desc"
    This reverts commit d864a34e3a3f2dc608e2abdd5aeee9e654925f44.

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -186,9 +186,7 @@ function Welcome() {
               <Typography variant={'h2'}>趋势转换 </Typography>
             </Badge>
 
-            <Typography variant={'body1'}>
-              实时跟踪趋势反转，风险越高越灵敏，同时风险越大。
-            </Typography>
+            <Typography variant={'body1'}>实时跟踪趋势反转</Typography>
           </Box>
 
           {trendingChane && (

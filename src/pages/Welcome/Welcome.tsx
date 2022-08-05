08acd78cae9524c5381245d1a7d513a4fabb1117
agdholo <agdholo@gmail.com>
2022-08-05 21:36:38 +0800

    feat(app): add btc trending

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -86,6 +86,9 @@ function Welcome() {
 
                         {trendingOverview && (
                             <Grid item xs={12} md={6}>
+                                <Typography variant={"subtitle2"}>
+                                    当前大盘偏{trendingOverview.coin.coinLong[0] ?? 0 > trendingOverview.coin.coinShort[0] ?? 0 ? '多' : '空'}： {trendingOverview.coin.coinLong[0] ?? 0} 多， {trendingOverview.coin.coinShort[0] ?? 0} 空
+                                </Typography>
                                 <Box height={'300px'}>
                                     {trendingOverview && (
                                         <DataGrid

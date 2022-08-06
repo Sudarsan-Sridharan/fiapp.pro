80999ed4291cf5ad99cfc094c5ed5f96759cb02f
agdholo <agdholo@gmail.com>
2022-08-06 15:39:11 +0800

    feat(coinSearch): add Skeleton

--- src/components/Search/Asynchronous.tsx
+++ src/components/Search/Asynchronous.tsx
@@ -7,6 +7,7 @@ import {domain, fetcher} from "@/ network/fether";
 import parse from 'autosuggest-highlight/parse';
 import match from 'autosuggest-highlight/match';
 import {useNavigate} from "react-router-dom";
+import {OutlinedInput, Skeleton} from "@mui/material";
 
 interface Film {
     name: string;
@@ -22,9 +23,11 @@ function sleep(delay = 0) {
 export default function Asynchronous() {
     const [open, setOpen] = React.useState(false);
     const [options, setOptions] = React.useState<readonly Film[]>([]);
-    const loading = open && options.length === 0;
 
     const {data} = useSWR(`${domain}/Coin`, fetcher)
+
+    const loading = open && options.length === 0;
+
 
     const nav = useNavigate()
 
@@ -40,58 +43,64 @@ export default function Asynchronous() {
     }, [data, open]);
 
     return (
-        <Autocomplete
-            id="asynchronous-demo"
-            sx={{width: 300}}
-            size={'small'}
-            open={open}
-            onOpen={() => {
-                setOpen(true);
-            }}
-            onClose={() => {
-                setOpen(false);
-            }}
-            isOptionEqualToValue={(option, value) => option.name === value.name}
-            getOptionLabel={(option) => option.name}
-            options={options}
-            loading={loading}
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label="搜索币种"
-                    InputProps={{
-                        ...params.InputProps,
-                        endAdornment: (
-                            <React.Fragment>
-                                {loading ? <CircularProgress color="inherit" size={20}/> : null}
-                                {params.InputProps.endAdornment}
-                            </React.Fragment>
-                        ),
+        <>
+            {data ? (
+                <Autocomplete
+                    id="asynchronous-demo"
+                    sx={{width: 300}}
+                    size={'small'}
+                    open={open}
+                    onOpen={() => {
+                        setOpen(true);
                     }}
-                />
-            )}
-            renderOption={(props, option, {inputValue}) => {
-                const matches = match(option.name, inputValue);
-                const parts = parse(option.name, matches);
+                    onClose={() => {
+                        setOpen(false);
+                    }}
+                    isOptionEqualToValue={(option, value) => option.name === value.name}
+                    getOptionLabel={(option) => option.name}
+                    options={options}
+                    loading={loading}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="搜索币种"
+                            InputProps={{
+                                ...params.InputProps,
+                                endAdornment: (
+                                    <React.Fragment>
+                                        {loading ? <CircularProgress color="inherit" size={20}/> : null}
+                                        {params.InputProps.endAdornment}
+                                    </React.Fragment>
+                                ),
+                            }}
+                        />
+                    )}
+                    renderOption={(props, option, {inputValue}) => {
+                        const matches = match(option.name, inputValue);
+                        const parts = parse(option.name, matches);
 
 
-                return (
-                    <li {...props} onClick={() => nav(`/d/${option.name}`)}>
-                        <div>
-                            {parts.map((part, index) => (
-                                <span
-                                    key={index}
-                                    style={{
-                                        fontWeight: part.highlight ? 700 : 400,
-                                    }}
-                                >
+                        return (
+                            <li {...props} onClick={() => nav(`/d/${option.name}`)}>
+                                <div>
+                                    {parts.map((part, index) => (
+                                        <span
+                                            key={index}
+                                            style={{
+                                                fontWeight: part.highlight ? 700 : 400,
+                                            }}
+                                        >
                   {part.text}
                 </span>
-                            ))}
-                        </div>
-                    </li>
-                );
-            }}
-        />
+                                    ))}
+                                </div>
+                            </li>
+                        );
+                    }}
+                />
+            ) : <Skeleton sx={{width: '300px'}}>
+                <OutlinedInput/>
+            </Skeleton>}
+        </>
     );
 }

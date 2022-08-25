42fbb09c139e50afdac4683364b42c668902dbc4
agdholo <agdholo@gmail.com>
2022-08-25 10:11:27 +0800

    fix(dayjs): ts import

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,8 +1,8 @@
 import dayjs from "dayjs";
-import * as relativeTime from 'dayjs/plugin/relativeTime'
+import RelativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/zh-cn'
 
-dayjs.extend(relativeTime)
+dayjs.extend(RelativeTime)
 dayjs.locale('zh-cn')
 
 export const timejs = dayjs

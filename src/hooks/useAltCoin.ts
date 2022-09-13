6c8348802b9a0b643a403f29fbc76cfe2c05bb4e
agdholo <agdholo@gmail.com>
2022-09-13 16:46:17 +0800

    chore(alt): add data type

--- src/hooks/useAltCoin.ts
+++ src/hooks/useAltCoin.ts
@@ -2,8 +2,13 @@ import useSWR from "swr";
 import {domain, fetcher} from "@/ network/fether";
 import {useEffect, useState} from "react";
 
+interface IAltCoin {
+    current_trending: number,
+    open_time: Date
+}
+
 const useAltCoin = () => {
-    const [data, setData] = useState<any>('')
+    const [data, setData] = useState<IAltCoin>()
 
     const {data: altCoinData} = useSWR(`${domain}/TrendingChange?name=ALT-PERP&timeframe=4H&risk=1`, fetcher, {
         refreshInterval: 1000 * 60 * 1,
--- src/pages/Welcome/_chart.tsx
+++ src/pages/Welcome/_chart.tsx
@@ -1,7 +1,6 @@
 import useSWR from "swr";
 import {domain, fetcher} from "@/ network/fether";
 import React from "react";
-import {useAPIQuery} from "@/hooks/useAPIQuery";
 import {Box, Button, Container, Grid, Paper, Stack, Tooltip, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
 import {List} from "linqts";
@@ -10,16 +9,6 @@ import KlineChart from "@/components/Chart/Kline";
 import {green, grey} from "@mui/material/colors";
 import useAltCoin from "@/hooks/useAltCoin";
 
-const coins = [
-    {name: 'BTCUSDT', displayName: 'BTC'},
-    {name: 'ETHUSDT', displayName: 'ETH'}
-]
-
-const CoinSelect = [
-    {title: 'BTC 比特币', name: 'BTCUSDT', subTitle: '30M - 空(5) 1H - 空(4)'},
-    {title: 'ETH 以太坊', name: 'ETHUSDT', subTitle: '30M - 空(4) 1H - 空(2)'},
-    {title: 'BNB 币安币', name: 'BNBUSDT', subTitle: '30M - 空(2) 1H - 空(2)'},
-]
 
 interface ITrendingChange {
     name: string;
@@ -30,28 +19,10 @@ interface ITrendingChange {
 }
 
 const Chart = () => {
-    const APIQuery = useAPIQuery()
-    const [name, setName] = React.useState('BTCUSDT')
-
-    const conditions = {
-        risk: '',
-        timeframe: APIQuery.value.timeframe ?? '',
-        currentTrending: '',
-        name: name
-    };
-    const sendUrl = new URLSearchParams(conditions).toString();
-
-
-    const {data: trendingChange} = useSWR(`${domain}/TrendingChange?${sendUrl}`, fetcher, {
-        refreshInterval: 1000 * 60 * 1,
-    });
-
-    const {data: riskWarning} = useSWR(`${domain}/RiskWarning?${sendUrl}`, fetcher, {
-        refreshInterval: 1000 * 60 * 1,
-    });
+    const name = 'BTCUSDT'
 
     const {data: coinsTC} = useSWR(`${domain}/TrendingChange?names=BTCUSDT,ETHUSDT,BNBUSDT,ATOMUSDT`, fetcher, {
-        refreshInterval: 1000 * 60 * 1,
+        refreshInterval: 1000 * 60,
     });
 
     const alt = useAltCoin()
@@ -158,7 +129,7 @@ const Chart = () => {
                     p: 1,
                 }} elevation={alt.currentTrending === 1 ? 1 : 0}>
                     <Typography variant={'body1'}>
-                        山寨季：{alt.currentTrending === 1 ? '开始' : alt.currentTrending === -1 ? '结束' : '中立'} - {timejs(alt.data.open_time).fromNow()}
+                        山寨季：{alt.currentTrending === 1 ? '开始' : alt.currentTrending === -1 ? '结束' : '中立'} - {timejs(alt.data?.open_time).fromNow()}
                     </Typography>
                 </Paper>)}
                 <KlineChart name={name}/>

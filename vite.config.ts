af42491f1a3b1196778fdc341a2003f2bec70206
agdholo <agdholo@gmail.com>
2022-09-15 10:53:37 +0800

    Revert "chore(app): upgrade deps"
    This reverts commit 1bb67c1d633e588b1c0a67aab354fb68a6770049.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import * as path from "path";
+import react from "@vitejs/plugin-react";
 import {defineConfig} from "vite";
 import {VitePWA} from "vite-plugin-pwa";
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
+
 import manifest from "./manifest.json";
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
     optimizeDeps: {},
     plugins: [
+        react({babel: {plugins: [jotaiDebugLabel, jotaiReactRefresh]}}),
         VitePWA({
             manifest,
             includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
